3891863cd25d3ede83c62f88bc60c5f0268ddaf5
Siba Sankar Gouda <sibasankargouda@Sibas-MacBook-Air.local>
2025-05-20 00:24:30 +0530

    counterApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,18 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular19';
+  title = 'Counter App';
+  counter = 0;
+  handleCount(val: string) {
+    if (val === 'plus') {
+      this.counter = this.counter + 1;
+    } else if (val === 'min') {
+      if (this.counter > 0) {
+        this.counter = this.counter - 1;
+      }
+    } else {
+      this.counter = 0;
+    }
+  }
+
 }
